9a3343c357fa41321d321712f90fcf45a6dadec7
michaelroscero <michaelceronio2005@gmail.com>
2025-04-18 08:19:34 -0500

    Cleaned up old static files after migrating to React

---
+++ my-portfoilo/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Typed from 'react-typed';
+import './style.css'; // move style.css here if needed
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/images/Headshot.jpg" alt="Profile" className="profile-picture" />
+      <h1 style={{ textAlign: "center" }}>
+        <Typed
+          strings={["Welcome to my portfolio!", "It's great to have you here!"]}
+          typeSpeed={50}
+          backSpeed={30}
+          loop
+        />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
